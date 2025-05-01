2f657971f507dd00dda2a38f933bfb71f2572fec
Imran Parthib <imranparthib13@gmail.com>
2025-05-02 00:34:15 +0600

    ✨ Add implementation for finding numbers with even digits in TypeScript and JavaScript

--- LeetCode/1295_Even_Numbers_Digits.ts
+++ LeetCode/1295_Even_Numbers_Digits.ts
@@ -1,2 +1,14 @@
-let firstNumber: number = 0;
-console.log(firstNumber);
+/**
+ * Find Numbers with Even Number of Digits
+ * Author: Imran Parthib
+ * Date: May 02, 2025
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumbers = (nums: number[]): number => {
+    return 0;
+};
+
+let nums = [12, 34, 56, 677, 777, 89];
+const result: number = findNumbers(nums);
+console.log(result)
--- LeetCode/out/1295_Even_Numbers_Digits.js
+++ LeetCode/out/1295_Even_Numbers_Digits.js
@@ -1,2 +1,19 @@
-var firstNumber = 0;
-console.log(firstNumber);
+/**
+ * Find Numbers with Even Number of Digits
+ * Author: Imran Parthib
+ * Date: May 02, 2025
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function (nums) {
+    var even = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 === 0) {
+            even++;
+        }
+    }
+    return even;
+};
+var nums = [12, 345, 2, 6, 7896];
+var result = findNumbers(nums);
+console.log(result);
